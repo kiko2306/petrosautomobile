de5ee673848681fd194adefb729e5fb83fcee7d4
Teixeira <Miguel1.Teixeira@kantar.com>
2024-02-12 16:30:30 +0000

    'dev'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    
+    $('#laptop').delay(10000).queue(function () {
+        $('#laptop').addClass('dc-itl');
+        $('smatphone').addClass('dc-itl');
+    });
 });
